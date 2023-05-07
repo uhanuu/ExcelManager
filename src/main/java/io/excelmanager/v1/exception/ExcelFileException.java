8dbb7681c2992ef1977fb15a931b0019cbd422be
uhanuu <hwryu990927@gmail.com>
2023-05-07 20:19:33 +0900

    feat: ExceptionHandler를 통한 간단한 예외처리

---
+++ src/main/java/io/excelmanager/v1/exception/ExcelFileException.java
@@ -0,0 +1,24 @@
+package io.excelmanager.v1.exception;
+
+public class ExcelFileException extends RuntimeException {
+
+    public ExcelFileException() {
+        super();
+    }
+
+    public ExcelFileException(String message) {
+        super(message);
+    }
+
+    public ExcelFileException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ExcelFileException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ExcelFileException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ src/main/java/io/excelmanager/v1/exception/exhandler/ErrorResult.java
@@ -0,0 +1,11 @@
+package io.excelmanager.v1.exception.exhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
