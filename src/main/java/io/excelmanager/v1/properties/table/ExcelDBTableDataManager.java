43d631e33e8ee1b68c31ae0ae85631a0b3ddaa7f
uhanuu <hwryu990927@gmail.com>
2023-05-10 13:36:19 +0900

    style: connection close() 처리

--- src/main/java/io/excelmanager/v1/properties/table/ExcelDBTableDataManager.java
+++ src/main/java/io/excelmanager/v1/properties/table/ExcelDBTableDataManager.java
@@ -5,10 +5,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -22,6 +19,7 @@ public class ExcelDBTableDataManager {
     {
         selectOperationTable.put(MODE_CONSTANT[0], new ExcelToTableCreator());
         selectOperationTable.put(MODE_CONSTANT[1], new ExcelToTableUpdater());
+        selectOperationTable.put(MODE_CONSTANT[2], new ExcelToTableDefault());
     }
     private ExcelToTableOperation ExcelToTableOperation;
     @Value("${excel.mode:default}")
@@ -40,27 +38,72 @@ public class ExcelDBTableDataManager {
         this.tableName = tableName;
         this.attributeKey = attributeKey;
         this.attributeType = attributeType;
-        log.info("TableOperation init ={}", ExcelToTableOperation);
     }
 
     @PostConstruct
-    public void executeTableOperation(){
+    public void executeTableOperation() throws SQLException {
         String checkValue = mode.toUpperCase();
         if (!modeCheck(checkValue)){
             throw new ModeNotFountException();
         }
+
+        if (checkValue.equals(MODE_CONSTANT[2])) return;
+
         ExcelToTableOperation = selectOperationTable.get(checkValue);
+        Connection connection = null;
+        Statement statement = null;
         try{
-            Connection connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
+            //get
+            connection = getConnection();
+            statement = connection.createStatement();
             ExcelToTableOperation.execute(statement,tableName,attributeKey,attributeType);
 
         } catch (SQLException e){
             e.printStackTrace();
+            throw e;
         } catch (NoSuchElementException e){
             log.error("application에서 DataType을 확인해주세요");
+        } finally {
+            //시작과 역순으로 close 해주기
+            close(connection,statement,null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet resultSet){
+
+        if (resultSet != null){
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                log.info("error",e);
+            }
         }
 
+        if (stmt != null){
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error",e);
+            }
+        }
+
+        if (con != null){
+            try {
+                con.close(); //Exception
+            } catch (SQLException e) {
+                log.info("error",e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        try{
+            Connection connection = DriverManager.getConnection(url, username, password);
+            log.info("get connection={}, class={}",connection,connection.getClass());
+            return connection;
+        } catch (SQLException e){
+            throw new IllegalStateException(e);
+        }
     }
 
     private static boolean modeCheck(String checkValue){
---
+++ src/main/java/io/excelmanager/v1/properties/table/ExcelToTableDefault.java
@@ -0,0 +1,12 @@
+package io.excelmanager.v1.properties.table;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class ExcelToTableDefault implements ExcelToTableOperation{
+    @Override
+    public void execute(Statement statement, String tableName, List<String> attributeKey, List<String> attributeType) throws SQLException {
+
+    }
+}
